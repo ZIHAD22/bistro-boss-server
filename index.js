bcbd42e56130569c683993f7d9d4b24c9798f38d
ZIHAD22 <mdzihad2244@gmail.com>
2024-02-23 01:21:10 +0600

    change db url

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+console.log(process.env.NODE_ENV);
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 // dataBase;
-const uri =
-  process.env.NODE_ENV === "development"
-    ? process.env.DEV_URL
-    : process.env.PRO_URL;
-
-console.log(uri);
+let uri;
+if (process.env.NODE_ENV === "development") {
+  uri = process.env.DEV_DB_URL;
+} else {
+  uri = process.env.PRO_DB_URL;
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
@@ -23,20 +24,24 @@ const client = new MongoClient(uri);
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    console.log("ok");
     await client.connect();
     // Send a ping to confirm a successful connection
 
     const menuCollection = client.db("Bistro-Boss").collection("menus");
     const reviewCollection = client.db("Bistro-Boss").collection("reviews");
+    const cartCollection = client.db("Bistro-Boss").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
-      console.log(result);
       res.send(result);
     });
     app.get("/review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const result = await cartCollection.insertOne(req.body);
       res.send(result);
     });
 
